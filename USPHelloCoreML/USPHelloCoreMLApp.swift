32e4204f3eeaaebbd13ebc67e99b031b4f07423b
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-10-22 23:35:25 +0900

    Initial Commit

---
+++ USPHelloCoreML/USPHelloCoreMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  USPHelloCoreMLApp.swift
+//  USPHelloCoreML
+//
+//  Created by joe on 10/22/25.
+//
+
+import SwiftUI
+
+@main
+struct USPHelloCoreMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
