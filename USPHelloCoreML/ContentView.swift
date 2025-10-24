d5ab2ccc7f7eac92e3192343e1435d495268b5cd
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-10-24 23:58:20 +0900

    integrate YOLOv3Tiny CoreML model for object detection
    - added YOLOv3Tiny.mlmodel to the project and updated ContentView to perform object detection using Vision and CoreML
    - introduced an Observation struct to store detected object data and updated the prediction workflow to handle object detection results

--- USPHelloCoreML/ContentView.swift
+++ USPHelloCoreML/ContentView.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 import CoreML
 import PhotosUI
+import Vision
+
+struct Observation {
+    let label: String
+    let confidence: VNConfidence
+    let boundingBox: CGRect
+}
 
 struct ContentView: View {
     @State private var probs: [String: Double] = [:]
     @State private var uiImage: UIImage? = UIImage(named: "stop-sign")
     @State private var selectedPhotoItem: PhotosPickerItem? = nil
     @State private var isCameraSelected: Bool = false
+    
+    let model = try! YOLOv3Tiny(configuration: MLModelConfiguration())
+    @State private var detectedObjects: [Observation] = []
     
     var body: some View {
         VStack {
@@ -33,7 +43,33 @@ struct ContentView: View {
             }
             
             Button("Predict") {
+                let mlModel = model.model
+                guard let coreMLModel = try? VNCoreMLModel(for: mlModel) else { return }
                 
+                let request = VNCoreMLRequest(model: coreMLModel) { request, error in
+                    guard let results = request.results as? [VNRecognizedObjectObservation] else { return }
+                    
+                    detectedObjects = results.map { result in
+                        guard let label = result.labels.first?.identifier else { return Observation(label: "", confidence: VNConfidence.zero, boundingBox: .zero) }
+                        let confidence = result.labels.first?.confidence ?? 0.0
+                        let boundingBox = result.boundingBox
+                        let observation = Observation(label: label, confidence: confidence, boundingBox: boundingBox)
+                        print(label)
+                        print("\(confidence)")
+                        print(boundingBox)
+                        return observation
+                    }
+                }
+                
+                guard let image = uiImage, let pixelBuffer = image.toCVPixelBuffer() else { return }
+                
+                let requestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer)
+                
+                do {
+                    try requestHandler.perform([request])
+                } catch {
+                    print(error.localizedDescription)
+                }
             }.buttonStyle(.borderedProminent)
         }
         .onChange(of: selectedPhotoItem) { _, selectedPhotoItem in
